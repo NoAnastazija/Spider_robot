1a015a27f949a01286332513a3d8c026640a29ce
Anastazija Nograšek <nograsek.nograsek@gmail.com>
2022-06-09 11:36:09 +0200

    Cleanup

--- Scaling_servos/Scaling_servos.ino
+++ Scaling_servos/Scaling_servos.ino
@@ -37,8 +37,6 @@
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMIN  102// This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  492 // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
 
