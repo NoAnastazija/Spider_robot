90f0cedf88f3b1011d3dc649d524ecab5010378a
Simon Rozman <simon@rozman.si>
2022-06-22 10:43:42 +0200

    rpi: initialize all joints at once

--- rpi/inicialization.py
+++ rpi/inicialization.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Moves selected servo to 90° position
+# Moves all servos to 90° position
 
 import time    #https://docs.python.org/fr/3/library/time.html
 from adafruit_servokit import ServoKit #https://circuitpython.readthedocs.io/projects/servokit/en/latest/
 
-pca = ServoKit(channels=16)
+NUMBER_OF_SERVOS = 16
 
-i = 7
-pca.servo[i].set_pulse_width_range(500, 2400)
-pca.servo[i].angle = 80
-time.sleep(1)
-pca.servo[i].angle = 100
-time.sleep(1)
-pca.servo[i].angle = 90
-time.sleep(1)
-#pca.servo[i].angle = None
+pca = ServoKit(channels=NUMBER_OF_SERVOS)
+
+for i in range(NUMBER_OF_SERVOS):
+    pca.servo[i].set_pulse_width_range(500, 2400)
+    pca.servo[i].angle = 80
+    time.sleep(1)
+    pca.servo[i].angle = 100
+    time.sleep(1)
+    pca.servo[i].angle = 90
+    time.sleep(1)
+    #pca.servo[i].angle = None
